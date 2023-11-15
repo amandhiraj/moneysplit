95bfb5403df40146f112704283c5f69e8066f3c5
amandhiraj <amandhiraj@live.ca>
2023-11-15 14:02:27 -0500

    added new gitignore

--- backend/moneysplit/src/main/java/com/moneysplit/money/Repository/ExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.moneysplit.money.Repository;
-
-import com.moneysplit.money.Model.Expense;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ExpenseRepository extends MongoRepository<Expense, String> {
-}
