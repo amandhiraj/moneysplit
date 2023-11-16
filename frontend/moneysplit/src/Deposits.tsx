6649e4fb02161871b5763d3c63661b9c0740960c
amandhiraj <amandhiraj@live.ca>
2023-11-16 00:12:19 -0500

    updated some files and added basic view templates

--- frontend/moneysplit/src/Deposits.tsx
+++ frontend/moneysplit/src/Deposits.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react';
+import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
+import Title from './Title';
 
-interface TitleProps {
-  children?: React.ReactNode;
+function preventDefault(event: React.MouseEvent) {
+  event.preventDefault();
 }
 
-export default function Title(props: TitleProps) {
+export default function Deposits() {
   return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
-      {props.children}
-    </Typography>
+    <React.Fragment>
+      <Title>Recent Deposits</Title>
+      <Typography component="p" variant="h4">
+        $3,024.00
+      </Typography>
+      <Typography color="text.secondary" sx={{ flex: 1 }}>
+        on 15 March, 2019
+      </Typography>
+      <div>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          View balance
+        </Link>
+      </div>
+    </React.Fragment>
   );
 }
--- frontend/moneysplit/src/Title.tsx
+++ frontend/moneysplit/src/Title.tsx
@@ -1,15 +1,14 @@
-# Pricing template
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
 
-## Usage
+interface TitleProps {
+  children?: React.ReactNode;
+}
 
-<!-- #default-branch-switch -->
-
-1. Copy the files into your project, or one of the [example projects](https://github.com/mui/material-ui/tree/master/examples).
-2. Make sure your project has the required dependencies: @mui/material, @mui/icons-material, @emotion/styled, @emotion/react.
-3. Import and use the `Pricing` component.
-
-## Demo
-
-<!-- #default-branch-switch -->
-
-View the demo at https://mui.com/material-ui/getting-started/templates/pricing/.
+export default function Title(props: TitleProps) {
+  return (
+    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+      {props.children}
+    </Typography>
+  );
+}
